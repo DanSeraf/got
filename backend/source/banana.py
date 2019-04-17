04913210ca1e4a3f31e973905a02dfdcd16867c6
DanSeraf <seraf.daniele@gmail.com>
2019-04-17 16:23:14 +0200

    user data submit endpoint

--- backend/source/banana.py
+++ backend/source/banana.py
@@ -14,27 +14,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
 
-assoc_table = db.Table('assoc_table',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.uid')),
-    db.Column('character_id', db.Integer, db.ForeignKey('characters.cid'))
-)
-
 class Users(db.Model):
+    __tablename__ = "users"
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(80))
     username = db.Column(db.String(80))
+    posted = db.Column(db.Boolean)
     
-    characters = db.relationship('Characters', secondary=assoc_table,
-            backref = db.backref('users'))
+    characters = db.relationship('Userscharacters', backref='users', lazy='dynamic')
 
     def __repr__(self):
-        return '%s, %s, %s' %(self.uid, self.email, self.username)
+        return '%s, %s, %s, %s' %(self.uid, self.email, self.username)
+
+class Userscharacters(db.Model):
+    __tablename__ = 'userscharacters'
+    ucid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80))
+    value = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.uid'))
+    
+    def __repr__(self):
+        return '%s, %s, %s' %(self.ucid, self.name, self.value)
 
 class Characters(db.Model):
+    __tablename__ = 'characters'
     cid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80))
     pic = db.Column(db.String(80))
-    value = db.Column(db.Integer)
 
     def __repr__(self):
         return '%s, %s, %s' %(self.cid, self.name, self.pic)
@@ -44,8 +51,7 @@ def initCharacters():
     f_str = file.read()
     characters_dict = json.loads(f_str)
     for name in characters_dict:
-        print(name)
-        new_character = Characters(name = name, pic = characters_dict[name], value=2)
+        new_character = Characters(name = name, pic = characters_dict[name])
         db.session.add(new_character)
 
 def mapDB():
@@ -57,12 +63,9 @@ def mapDB():
 def newUser():
     uname = request.args.get('username')
     email = request.args.get('email')
-    user = Users(email = email, username = uname)
+    user = Users(email = email, username = uname, posted=False)
     db.session.add(user)
     db.session.commit()
-    #q = Users.query.filter_by(email = email).first()
-    #print(q.__dict__)
-    #for i in range(1,35):
     return jsonify({'status': 'ok'})
 
 @app.route('/characters/all', methods=['GET'])
@@ -70,22 +73,43 @@ def allCharacters():
     qres = Characters.query.all()
     return json.dumps(qres, cls=AlchemyEncoder)
 
-@app.route('/user/get_username', methods=['POST'])
+@app.route('/user/get-username', methods=['POST'])
 def getUsername():
     email = request.args.get('email')
-    qres = Users.query.filter_by(email = email).all()
-    res = qres[0].__dict__
+    user = Users.query.filter_by(email = email).all()
+    res = user[0].__dict__
     username = res['username']
-    print(username)
-    return json.dumps(username)
+    posted = res['posted']
+    return json.dumps({'username': username, 'posted': posted})
 
-@app.route('/user/get/', methods=['GET'])
-def getUser():
-    qres = Users.query.filter_by(username = 'nyme').first()
+@app.route('/user/<username>', methods=['GET'])
+def getUser(username):
+    qres = Users.query.filter_by(username = username).first()
     return json.dumps(qres, cls=AlchemyEncoder)
 
-#@app.route('/selected', methods=['POST'])
-#def postForm():
+@app.route('/user/post_data', methods=['POST'])
+def postData():
+    import ast 
+    raw = request.args.get('data')
+    data = ast.literal_eval(raw)
+    for k in data:
+        print(k)
+        uname = k
+        raw_d = data[k]
+        q = Users.query.filter_by(username = uname).first()
+        for k in raw_d:
+            print(k)
+            to_add = Userscharacters(name=k, value=raw_d[k])
+            q.characters.append(to_add)
+    
+    user = Users.query.filter_by(username = uname).first()
+    user.posted = True
+    db.session.commit() 
+
+    return json.dumps({'status':'ok'})
+
+#@app.route('/user/posted', methods=['POST'])
+#def posted():
 
 
 if __name__ == '__main__':
