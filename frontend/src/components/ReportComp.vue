2f0fed81e4e41474ece49173ce92593a5ae0b10e
Xirver <federico.casenove@gmail.com>
2019-04-26 15:26:49 +0200

    graphics game sheet

--- frontend/src/components/ReportComp.vue
+++ frontend/src/components/ReportComp.vue
@@ -5,7 +5,7 @@
         <v-card flat class="text-xs-center " color="#2096f3" style="border-radius: 8px;" >
           <br>
           <v-card-title flat class="text-xs-center"><h2>{{ username }}'s Game Sheet</h2></v-card-title>
-          <v-card flat class="text-xs-center ma-3" :color="color[sheet.value]" :key="sheet.ucid" v-for="sheet in gamesheet">
+          <v-card flat class="text-xs-center ma-3" :color="colors[sheet.value]" :key="sheet.ucid" v-for="sheet in gamesheet">
             <v-card-text flat class="text-xs-center">
               {{ sheet.name }} - {{ live[sheet.value] }}
             </v-card-text>
@@ -25,7 +25,7 @@ export default {
     gamesheet: null,
     username: '',
     live: ['DIE', 'LIVE'],
-    color: ['red', 'green']
+    colors: ['red', 'green']
   }),
 
   methods:{
